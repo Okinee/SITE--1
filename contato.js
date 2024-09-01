67fb565ef3103506d38187858c327b19cfbf4d65
Kauã.scs <Kauafelipescs@gmail.com>
2024-09-01 00:04:12 -0300

    Pronto

--- contato.js
+++ contato.js
@@ -7,3 +7,12 @@ url === contato.html
 function AbrirContatos(){
     window.open("contato.html", '_blank')
 }
+
+function SobreMais(){
+    window.open("Sobre.html", '_self')
+}
+
+window.addEventListener("scroll", function(){
+    let header = document.querySelector('.container1')
+    header.classList.toggle('rolagem' , window.scrollY>100)
+})
